6697389c6ef680222def93fd4def4ee3163b866f
Jack A. Harris Jr <ldigitaldash@gmail.com>
2012-02-20 19:21:24 -0500

    Just added a function addRecord

--- struct.js
+++ struct.js
@@ -10,6 +10,12 @@
         var $this = $(this);
         $this.append(inputBoxes(settings));
         $this.append(tables(settings));
+        $('#' + settings['prefix'] + "Button").click(function() {
+            var key, value;
+            key = $('#' + settings['prefix'] + 'Key').val();
+            value = $('#' + settings['prefix'] + 'Value').val();
+            
+        });
     });
     // Do your awesome plugin stuff here
 
@@ -22,12 +28,19 @@ function inputBoxes(settings) {
     elements = elements + "<input type='text' id='" + settings['prefix'] + "Key'></input>";
     elements = elements + "<label>" + settings['value'] + "</label>";
     elements = elements + "<input type='text' id='" + settings['prefix'] + "Value'></input>";
+    elements = elements + "<span id='"+ settings['prefix']  +"Button'>Add</span>"
     return elements;
 }
 
 function tables(settings) {
     elements = "<table id='" + settings['prefix'] + "Table'>";
-    elements = elements + "<thead><th>" + settings['key'] + "</th><th>" + settings['value'] + "</th></thead>";
+    elements = elements + "<thead><th>" + settings['key'] + "</th><th>" + settings['value'] + "</th><th>&nbsp;</th></thead>";
     elements = "<tbody></tbody></table>";
+    return elements;
+}
+
+function addRecord(settings, key, value) {
+    elements = "<tr><td>" + key + "</td><td>" + value + "</td>";
+    elements = elements + "<td><span></span></td></tr>";
     return elements;
 }
